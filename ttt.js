1853c3f511f1c9979aca67e8a41402b3d6f95145
edwindixon99 <49737629+edwindixon99@users.noreply.github.com>
2021-08-23 19:27:02 +1200

    slight changes taking break

--- ttt.js
+++ ttt.js
@@ -8,7 +8,16 @@ const GameBoard = (() => {
 
 Player
 const GameManager = (() => {
-    const hasWon = GameBoard.board
+    const hasWon = function() {
+        let row = GameBoard.board.some( array => array.every( (val, i, arr) => val === arr[0] ) ) 
+        let col = function() {
+            let first = null;
+            colDone = false;
+            for (let i = 0; i < GameBoard.board; i++) {
+                first = GameBoard.board[i][0]
+                for (let j = 0; j < )
+            } 
+
 }
 
 
